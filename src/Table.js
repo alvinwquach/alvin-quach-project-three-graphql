934732612611eccd8ff0812de1fd41118c55af0c
Alvin Quach <alvinwquach@gmail.com>
2021-11-24 00:53:03 -0800

    fixed table

--- src/Table.js
+++ src/Table.js
@@ -1,18 +1,20 @@
 const Table = ({ expenses }) => {
   return (
     <div>
+      <table>
       <tbody>
         {expenses.map((expense) => {
           return (
             <tr>
               <td>{expense.date}</td>
               <td>{expense.transaction}</td>
-              <td>{expense.amount}</td>
+              <td>${expense.amount}</td>
               <td>{expense.category}</td>
             </tr>
           );
         })}
       </tbody>
+      </table>
     </div>
   );
 };
